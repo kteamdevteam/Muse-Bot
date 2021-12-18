aea83fdefd1311060e5d2cd42497e4993aa8d48c
Kteam <93255148+kteamdevteam@users.noreply.github.com>
2021-12-18 20:55:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import music
 
 cogs = [music]
 
-client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
+client = commands.Bot(command_prefix='your pefix', intents = discord.Intents.all())
 
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
 
-client.run("OTIxNzQyOTk1MDA1NTA1NjA3.Yb3WDw.cHFqFNH8LKPMi1L8vXQTpXZH5m0")
+client.run("bot token")
